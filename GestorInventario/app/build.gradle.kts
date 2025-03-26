bb865e2407e5c4f449bafae20531a6040fa11d02
Fran Santana <santanafran96@gmail.com>
2025-03-26 14:50:19 +0100

    modificaciones 26-03

--- GestorInventario/app/build.gradle.kts
+++ GestorInventario/app/build.gradle.kts
@@ -42,6 +42,8 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
